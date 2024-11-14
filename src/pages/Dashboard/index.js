bbd4e5a9919a6058cfb3933ee4fc9ca9a2e3bced
geovana <geovanasslima@gmail.com>
2024-11-14 09:11:45 -0300

    adding content on dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,12 @@
+import './dashboard.css';
+import Sidebar from "../../components/Sidebar";
+import Title from "../../components/Title";
+
 import { useContext } from "react"
 import { AuthContext } from "../../contexts/auth"
+import { FiEdit2, FiMessageSquare, FiPlus, FiSearch } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
-import Sidebar from "../../components/Sidebar";
 
 export default function Dashboard() {
   const { logOut } = useContext(AuthContext);
@@ -15,8 +20,47 @@ export default function Dashboard() {
 
       <Sidebar />
 
-      <h1>dash</h1>
-      <button onClick={handleLogOut}>Sair</button>
+      <div className="content">
+        <Title titleName="Tickets">
+          <FiMessageSquare size={30} />
+        </Title>
+
+        <>
+          <Link to="/new" className="new">
+            <FiPlus size={25} />
+            Novo chamado
+          </Link>
+
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Cliente</th>
+                <th scope="col">Assunto</th>
+                <th scope="col">Status</th>
+                <th scope="col">Cadastrado em</th>
+                <th scope="col">#</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td data-label="Cliente">Empresa Teste</td>
+                <td data-label="Assunto">Suporte</td>
+                <td data-label="Status">Em Aberto</td>
+                <td data-label="Cadastrado">12/11/2024</td>
+                <td data-label="#">
+                  <button className="action" style={{ backgroundColor: "#3583F6" }}>
+                    <FiSearch color="#FFF" size={17} />
+                  </button>
+                  <button className="action" style={{ backgroundColor: "#F6A935" }}>
+                    <FiEdit2 color="#FFF" size={17} />
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      </div>
     </div>
   )
 }
